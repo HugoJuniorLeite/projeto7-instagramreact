384b1fafc6ebdef9ed00d82a479e6a5846ba9115
Hugo junior <hjunioreng@gmail.com>
2022-11-15 21:22:35 -0300

    transformando codigo HTML para React

---
+++ src/Sugestoes.js
@@ -0,0 +1,71 @@
+export default function Sugestoes(){
+
+return(
+<div class="sugestoes">
+<div class="titulo">
+  Sugestões para você
+  <div>Ver tudo</div>
+</div>
+
+<div class="sugestao">
+  <div class="usuario">
+    <img src="assets/img/bad.vibes.memes.svg" alt="a" />
+    <div class="texto">
+      <div class="nome">bad.vibes.memes</div>
+      <div class="razao">Segue você</div>
+    </div>
+  </div>
+
+  <div class="seguir">Seguir</div>
+</div>
+
+<div class="sugestao">
+  <div class="usuario">
+    <img src="assets/img/chibirdart.svg" alt="a" />
+    <div class="texto">
+      <div class="nome">chibirdart</div>
+      <div class="razao">Segue você</div>
+    </div>
+  </div>
+
+  <div class="seguir">Seguir</div>
+</div>
+
+<div class="sugestao">
+  <div class="usuario">
+    <img src="assets/img/razoesparaacreditar.svg" alt="a" />
+    <div class="texto">
+      <div class="nome">razoesparaacreditar</div>
+      <div class="razao">Novo no Instagram</div>
+    </div>
+  </div>
+
+  <div class="seguir">Seguir</div>
+</div>
+
+<div class="sugestao">
+  <div class="usuario">
+    <img src="assets/img/adorable_animals.svg" alt="a"/>
+    <div class="texto">
+      <div class="nome">adorable_animals</div>
+      <div class="razao">Segue você</div>
+    </div>
+  </div>
+
+  <div class="seguir">Seguir</div>
+</div>
+
+<div class="sugestao">
+  <div class="usuario">
+    <img src="assets/img/smallcutecats.svg" alt="a" />
+    <div class="texto">
+      <div class="nome">smallcutecats</div>
+      <div class="razao">Segue você</div>
+    </div>
+  </div>
+
+  <div class="seguir">Seguir</div>
+</div>
+</div>
+)
+}
---
+++ src/Usuario.js
@@ -0,0 +1,15 @@
+export default function usuario(){
+
+return(
+<div class="usuario">
+<img src="assets/img/catanacomics.svg" alt="a" />
+<div class="texto">
+  <strong>catanacomics</strong>
+  <span>
+    Catana
+    <ion-icon name="pencil"></ion-icon>
+  </span>
+</div>
+</div>
+)
+}
