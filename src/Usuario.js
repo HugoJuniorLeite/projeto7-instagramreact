0f47dacf3437d3cfddf9d74e098ecaf3212e7cba
Hugo junior <hjunioreng@gmail.com>
2022-11-20 19:17:46 -0300

    correções de erro string vazia name

--- src/Usuario.js
+++ src/Usuario.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 
 export default function Usuario() {
-  const [nome, setNome] = useState("Catana")
-const [imgage, setImage] =useState ('assets/img/catanacomics.svg')
+const [nome, setNome] = useState("Catana")
+const [image, setImage] =useState ('assets/img/catanacomics.svg')
   
   function atualizarNome(){
 
-    setNome(prompt('escolha seu nome'))
-  }
+  setNome(prompt('escolha seu nome'))
+  
+}
 
   function atualizarImage(){
   setImage(prompt("Digite o link da imagem"))    
@@ -15,11 +16,11 @@ const [imgage, setImage] =useState ('assets/img/catanacomics.svg')
 
   return (
     <div data-test="user" className="usuario">
-      <img data-test="profile-image"onClick={atualizarImage} src={imgage} alt="a" />
+      <img data-test="profile-image"onClick={atualizarImage} src={!image ? 'assets/img/catanacomics.svg' : image} alt="a" />
       <div className="texto">
         <strong>catanacomics</strong>
         <span data-test="name">
-          { nome==="" ? {useState}: {nome}}
+          {!nome?"Catana" :nome}
           <ion-icon data-test="edit-name" onClick={atualizarNome} name="pencil"></ion-icon>
         </span>
       </div>
